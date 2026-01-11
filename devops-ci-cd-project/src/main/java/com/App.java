5fc81662c7261405a20e1028d06f4746f1d8ccc8
Ubuntu <ubuntu@ip-172-31-38-24.ap-south-1.compute.internal>
2026-01-11 16:53:29 +0000

    first commit

---
+++ devops-ci-cd-project/src/main/java/com/App.java
@@ -0,0 +1,10 @@
+package com.devops;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello DevOps CI/CD Pipeline!");
+        System.out.println("Build → SonarQube → Docker → Deploy");
+    }
+}
+
